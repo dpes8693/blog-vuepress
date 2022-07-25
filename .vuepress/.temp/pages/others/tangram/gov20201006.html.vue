1c708bd6b8021ec0039fc2d724406dbcead91fd1
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-07-25 10:10:36 +0800

    remove .cache .temp files from repo
    https://stackoverflow.com/questions/25436312/gitignore-not-working

--- .vuepress/.temp/pages/others/tangram/gov20201006.html.vue
+++
@@ -1,4 +0,0 @@
-<template><h1 id="_20201029-企業智動化與數位轉型研討會"><a class="header-anchor" href="#_20201029-企業智動化與數位轉型研討會">#</a> 20201029-企業智動化與數位轉型研討會</h1>
-<p><img src="https://drive.google.com/uc?export=download&amp;id=1T4FQtQc1sHN-Kghio8zjTEP7nrlVicFb" alt="背板">
-<img src="https://drive.google.com/uc?export=download&amp;id=1XJ5sDOsMicGCWEnbUP9FjePWxiQPU1Gr" alt="海報"></p>
-</template>
--- .vuepress/.temp/pages/portfolio/tangram-ms20201006.html.vue
+++
@@ -1,6 +0,0 @@
-<template><h1 id="_20201006微軟新創加速器-demo-day"><a class="header-anchor" href="#_20201006微軟新創加速器-demo-day">#</a> 20201006微軟新創加速器 DEMO Day</h1>
-<p><img src="https://drive.google.com/uc?export=download&amp;id=17fV5sI830l9HMSi4SETPPh15RHZ4b4Wg" alt="線上展設計稿">
-<img src="https://drive.google.com/uc?export=download&amp;id=16Al-K5BHLg14HtdDR2na4oL4pYFnGigv" alt="下">
-<img src="https://drive.google.com/uc?export=download&amp;id=1WOHu_XBlpqHHbnDgB9lzM-c7wvphDhzo" alt="左">
-<img src="https://drive.google.com/uc?export=download&amp;id=1-uic88_xT-_ePMos0OUAIKeRVj8vEiIE" alt="上"></p>
-</template>
