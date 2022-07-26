b94a4fa239bf2d4107a8c7f3af1a90e6e3e7ede4
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-07-26 15:13:52 +0800

    Add new pages

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,23 +3,24 @@ module.exports = {
   lang: "zh-tw",
   title: "Richard's Blog",
   head: [
-    [ "link", 
-      { 
-        rel: "icon", href: "/blog-vuepress/images/icon.png",
- 
-      }
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/blog-vuepress/images/icon.png",
+      },
     ],
-    ["script",
-      { 
-        src: "https://kit.fontawesome.com/e6a339ae70.js",crossorigin: "anonymous",
-      }
-    ]
-    // ["style",{ src: "/blog-vuepress/style/base.css"}]
+    [
+      "script",
+      {
+        src: "https://kit.fontawesome.com/e6a339ae70.js",
+        crossorigin: "anonymous",
+      },
+    ],
   ],
   dest: "docs",
   base: "/blog-vuepress/",
   description: "用Vupress架Blog",
-//
 
   themeConfig: {
     //logo: 'https://vuejs.org/images/logo.png',
@@ -37,55 +38,65 @@ module.exports = {
       // },
       //'/guide/1.md',
     ],
-
+    //會去抓#的名稱
     sidebar: [
       {
         isGroup: true,
-        text: 'Home',
-        link: '/',
-        // children: [
-        //   {
-        //     text: "github原始碼",
-        //     link: "https://github.com/dpes8693/blog-vuepress",
-        //     children: [],
-        //   },
-        // ],
+        text: "Home",
+        link: "/",
       },
       {
         isGroup: true,
         text: "Guide",
-        children: ["/guide/buildBlog.md","/guide/markdown.md","/guide/getll.md"], //會去抓#的名稱
+        children: [
+          "/guide/buildBlog.md",
+          "/guide/markdown.md",
+          "/guide/getll.md",
+          "/guide/regex.md",//未完成
+          "/guide/pattern.md",//未完成
+        ],
       },
       {
         isGroup: true,
         text: "Tool",
-        children: ["/tools/vscode.md","/tools/url.md"]
+        children: ["/tools/vscode.md", "/tools/url.md"],
       },
       {
         isGroup: true,
         text: "Web",
-        children: ["/web/common.md", "/web/resource.md", "/vue/vue.md", "/react/react.md","/web/keycode.md"]
+        children: [
+          "/web/common.md",
+          "/web/resource.md",
+          "/vue/vue.md",
+          "/react/react.md",
+          "/web/keycode.md",//0726完成
+          "/web/[JS]new.md",//未完成
+        ],
       },
       {
         isGroup: true,
         text: "Backend",
-        children: ["/backend/noun.md","/backend/express/index.md","/backend/normalization.md"]
+        children: [
+          "/backend/noun.md",
+          "/backend/express/index.md",
+          "/backend/normalization.md",
+        ],
       },
       {
         isGroup: true,
         text: "Windows",
-        children: ["/others/bashCommonSyntax.md"]
+        children: ["/others/bashCommonSyntax.md"],
       },
       {
         isGroup: true,
         text: "Proper Noun",
-        children: ["/web/noun.md",]
+        children: ["/web/noun.md"],
       },
       {
         isGroup: true,
         text: "Portfolio",
-        children: ["/portfolio/index.md"]   
-      }
+        children: ["/portfolio/index.md"],
+      },
     ],
   },
 };
